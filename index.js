70a37ce8a267a6ff48ef4c57f9903af4de38f8c6
deevarzariu <dell.varzariu@gmail.com>
2024-08-10 21:17:51 +0300

    Display req.body in morgan

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const persons = require("./persons.json");
 
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
+
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/info", (req, res) => {
   res.send(`
