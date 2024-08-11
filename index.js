081f96b161333b8c88034fafa7364318be04d9f9
deevarzariu <dell.varzariu@gmail.com>
2024-08-11 21:18:38 +0300

    Add cors middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const persons = require("./persons.json");
 
@@ -8,6 +9,7 @@ morgan.token("body", (req) => {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
